ef762b97538c9a952277f802498ef9695532a354
nhutphong <nhutphong@outlook.com>
2020-12-02 10:30:14 +0700

    2-12

--- shell/app.sh
+++ shell/app.sh
@@ -2,4 +2,4 @@
 
 #sudo apt install -y kali-defaults kali-root-login desktop-base kde-plasma-desktop kwrite dolphin vim-gtk3
 
-sudo apt install -y tree translate-shell vlc libreoffice okular tor torbrowser-launcher pandoc texlive-xetex
+sudo apt install -y tree translate-shell ripgrep fzf vlc libreoffice okular tor torbrowser-launcher pandoc texlive-xetex xclip alacarte
