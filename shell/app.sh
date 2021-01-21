93519310056428f0106ec87d433f555ad4133f0f
nhutphong <nhutphong@outlook.com>
2021-01-21 11:08:13 +0700

    21-1

--- shell/app.sh
+++ shell/app.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/zsh
 
-#sudo apt install -y kali-defaults kali-root-login desktop-base kde-plasma-desktop kwrite dolphin vim-gtk3
+sudo apt update && sudo apt dist-upgrade -y && sudo apt autoremove -y
 
-sudo apt install -y tree translate-shell ripgrep fzf vlc libreoffice okular tor torbrowser-launcher pandoc texlive-xetex xclip alacarte
+sudo apt install -y tree htop translate-shell ripgrep fzf apt-transport-https fonts-powerline python3-pip vlc libreoffice okular tor torbrowser-launcher pandoc texlive-xetex xclip ibus-unikey  
+
