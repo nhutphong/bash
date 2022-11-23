3fcc33f52f059a07bbbcf8afe71ee5d987f3d110
nhutphong <nhutphong@outlook.com>
2022-11-23 10:58:49 +0700

    modify

--- shell/pipe.sh
+++ shell/pipe.sh
@@ -1,36 +1,42 @@
-
-
-new file:
-touch file_name
-
+#!usr/bin/bash
 
 #pipeline
 
-"""
-	stdin=0
-	stdout=1
-	stderr=2
+: `
+  cmd thuong co 3 thu
+    stdin=0 is content=strings ,filename, la data dau vao cho cmd
+    stdout=1 is output ra terminal
+    stderr=2 is output ra terminal
+`
 
-"""
 redirection:
 
 stdout_cmd nay la stdin_cmd kia: giua 2 cmd
-stdout_cat cho stdin_grep
+stdout_cat is input cho stdin_grep
 cat test.sh | grep "echo"
 
 
 output test.sh cho input_grep: giua cmd va file
 grep "echo" < test.sh # chi dung duoc file
+grep "echo" test.sh # chi dung duoc file, khong can tag= '<'
 
 
-code trong terminal xuat ra output.log
-cmd <<STOP > output.log
-line1
-line2
-line3
+#  heredoc> co the hieu la buffer-content=filecodesh temp de cho cmd truoc dung
+#  code from line29 to line32 vao file out.sh
+#  render backtic `cmd`, $cmd, $(cmd)
+cat <<STOP > out.sh
+#!usr/bin/bash
+echo $HOME
+`js`
+`pwd`
 STOP
-# ctrl+d
 
+# argument la quote or double quote la "raw string, not render varname  
+cat <<"EOF"
+$(echo Hello)
+$(whoami)
+$PWD
+EOF
 
 
 output_cmd vao file
@@ -39,9 +45,9 @@ cmd 1> out.log # output vao out.log
 
 cmd > out.log 2>&1 # error vao out.log
 
-
-stdout vao stderr vao out.log
-cmd > out.log 2> out.log
+# > ==== 1>, dung > cho nhanh
+stdout-stderr of cmd vao out.log
+cmd > out.log 2> out.log      # dung '>' cho nhanh
 cmd 1> out.log 2> out.log
 
 
@@ -49,13 +55,11 @@ cmd > /dev/null 2>&1
 
 
 ls -l video.mpg blah.foo > myoutput 2>&1
-ls &> file_name => stdout and stderr vao file_name
+ls &> file_name		 stdout and stderr vao file_name
 
 
-ls 2>&1 => error_ls vao out_ls chinh no ko tao file
-ls >&2 => output tranh tao file 
+ls 2>&1	     error_ls vao out_ls chinh no ko tao file
+ls >&2	     output tranh tao file 
 
-
-echo "xin cho linux" >> file_name(append content)
-
-
+#append content
+echo "xin cho linux" >> output.log
